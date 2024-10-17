28e734c271bde57bf632a2378323ab7fa8738b56
Sala G4 <CSalaG4@uniboyaca.edu.co>
2024-10-17 18:10:46 -0500

    COnfiguracion de php

---
+++ db/crearcontacto2.php
@@ -0,0 +1,17 @@
+<?php
+require 'database/ejemplo_db.php';
+
+$ejemploDb = new EjemploDb();
+
+$sql = "insert into contactos (nombre,email,telefono)values";
+$sql .= "('Juanita','juanita@juanita.com','9876333')";
+
+$resultDB = $ejemploDb->query($sql);
+
+if ($resultDB) {
+    echo 'Datos guardados';
+} else {
+    echo 'No se pudo guardar la informacion';
+}
+
+$ejemploDb->close();
---
+++ db/modificarcontacto2.php
@@ -0,0 +1,20 @@
+<?php
+require 'database/ejemplo_db.php';
+
+$ejemploDb = new EjemploDb();
+
+$sql = "update contactos set ";
+$sql .= "nombre='pablito', ";
+$sql .= "email='pablito@pablito.com', ";
+$sql .= "telefono='654789' ";
+$sql .= " where id=1"; 
+
+$resultDB = $ejemploDb->query($sql);
+
+if ($resultDB) {
+    echo 'Datos guardados';
+} else {
+    echo 'No se pudo guardar la informacion';
+}
+
+$ejemploDb->close();
